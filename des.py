1fd952e85a32b13fda30902738068847e1bf0998
dustcat <takethispie@gmail.com>
2018-12-11 21:29:05 +0100

    fix DoPc1 now working

--- des.py
+++ des.py
@@ -7,7 +7,8 @@ class Des:
     def DoPC1(self, key64):
         key56 = ""
         for i in self.constDes["CP_1"]:
-            key56 += key64[i-1]
+            for j in i:
+                key56 += key64[j]
         return key56
 
     def SplitInTwo(self, key56):
@@ -21,7 +22,8 @@ class Des:
     def DoCompression(self, key56):
         key48 = ""
         for i in self.constDes["CP_2"]:
-            key48 += key56[i-1]
+            for j in i:
+                key48 += key56[j]
         return key48
 
 
