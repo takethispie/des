aed6ef0a58748f0e42a2053925cafd69d8be52d6
dustcat <takethispie@gmail.com>
2018-12-11 22:34:50 +0100

    wip Feistel function class
    wip sbox class

---
+++ sbox.py
@@ -0,0 +1,27 @@
+import textwrap
+
+
+def SplitTo6Bits(bits):
+    return textwrap.wrap(bits, 6)
+
+def FirstAndLastBit(bits6):
+    return bits6[0] + bits6[-1]
+
+
+def MiddleFourBits(bits6):
+    return bits6[1:5]
+
+
+def BinToDec(binarybits):
+    return int(binarybits, 2)
+
+
+def DecToBin(decimal):
+    return bin(decimal)[2:].zfill(4)
+
+
+def sbox_lookup(sboxMatrix, firstlast, middle4):
+    firstlast = BinToDec(firstlast)
+    middle = BinToDec(middle4)
+    res = sboxMatrix[firstlast][middle]
+    return DecToBin(res)
